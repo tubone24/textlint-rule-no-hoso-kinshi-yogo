eeccbfbccf245b6b257b499506bfdce482ce7562
tubone(Yu Otsubo) <meitantei-conan@live.jp>
2022-04-24 11:48:42 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const fs = require('fs');
 import { tokenize } from "kuromojin";
 import os from 'os';
 
-const dictionaryUrl = 'http://monoroch.net/kinshi/housouKinshiYougo.xml';
+const dictionaryUrl = 'https://raw.githubusercontent.com/tubone24/textlint-rule-no-hoso-kinshi-yogo/master/housouKinshiYougo.xml';
 const referenceUrl = 'http://monoroch.net/kinshi/';
 const dictionaryPath = `${os.tmpdir()}/housouKinshiYougo.xml`;
 const maxAge = 604800;
